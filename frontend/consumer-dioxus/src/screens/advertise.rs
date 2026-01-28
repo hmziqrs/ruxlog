cee423314f632d3757023be3e93e48d88d710170
hmziqrs <hmziqrs@gmail.com>
2026-01-28 06:31:52 +0500

    Standardize consumer static screen layouts

--- frontend/consumer-dioxus/src/screens/advertise.rs
+++ frontend/consumer-dioxus/src/screens/advertise.rs
@@ -3,8 +3,8 @@ use dioxus::prelude::*;
 #[component]
 pub fn AdvertiseScreen() -> Element {
     rsx! {
-        div { class: "container mx-auto px-4 py-8",
-            div { class: "max-w-3xl mx-auto",
+        div { class: "min-h-screen bg-background",
+            div { class: "container mx-auto px-4 py-8 md:py-12 lg:py-16 max-w-6xl",
                 h1 { class: "text-4xl font-bold mb-6", "Advertise with Ruxlog" }
                 div { class: "prose dark:prose-invert max-w-none",
                     p { "Advertising options will be published here. Get in touch to learn more." }
